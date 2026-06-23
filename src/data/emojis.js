805eaafd94d0c229b60a16aeb2bc0d8461359f50
Lucsc <lschmitt@exaduo.fr>
2026-06-23 18:05:56 +0200

    new emojis

--- src/data/emojis.js
+++ src/data/emojis.js
@@ -56,7 +56,7 @@ export const EMOJIS = [
     {
         id: 10,
         name: "Ludovic",
-        emojiSimple: ["🏈", "🤑", "🐟"],
+        emojiSimple: ["🏈", "🤑", "🐟", "🍈", "🎾"],
         emojiCompliquer: ["🩼", "💔", "🚬"],
     },
     {
@@ -80,7 +80,7 @@ export const EMOJIS = [
     {
         id: 14,
         name: "Olivier",
-        emojiSimple: ["🚲", "🪴", "🤑", "🐟"],
+        emojiSimple: ["🚲", "🪴", "🤑", "🐟", "🍈", "🎾"],
         emojiCompliquer: ["🩼", "🚗", "🚬", "⛷️"],
     },
     {
